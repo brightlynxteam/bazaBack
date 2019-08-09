5659626a5c5aec77de428d8ab51063e62f66d558
AleksDolgop <dolopolov9@gmail.com>
2019-08-10 02:45:04 +0700

    update findUsers (fixes#10)

--- src/server/db/queries/users.js
+++ src/server/db/queries/users.js
@@ -7,6 +7,22 @@ function getOneUser(data) {
         .select('id','email','phone_number','first_name','second_name', 'created_at', 'updated_at');
 }
 
+function getFindUsers(data) {
+    return knex('users')
+        .where({
+            //временная похабщина
+            second_name: data.queryString.split(' ')[0],
+            first_name: data.queryString.split(' ')[1],
+            phone_number: data.queryString.split(' ')[2]
+            //------------------
+        })    
+        .select('id','phone_number','email','first_name','second_name', 'created_at', 'updated_at')
+        .orderBy(data.orderBy, data.order)
+        .limit(data.limit)
+        .offset(data.offset);
+}
+
 module.exports = {
     getOneUser,
+    getFindUsers,
 };
