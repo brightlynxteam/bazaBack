43de77a625c2cf5ab85ff8c9df9180b8326b7b87
AleksDolgop <dolopolov9@gmail.com>
2019-08-10 02:44:08 +0700

    Revert "update findUsers (fixes #10)"
    This reverts commit e3315ad8c6f6fcc95c9aa0c1a3967cc089b2974e.

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -5,9 +5,7 @@ const usersQueries = require('../db/queries/users');
 const router = new Router();
 
 const PREFIX_URL = '/user';
-
 const GET_ONE_USER_URL = `${PREFIX_URL}`;
-const GET_FIND_USERS_URL = `${PREFIX_URL}/findUsers`
 
 router.post(GET_ONE_USER_URL,
     validator.validate(validator.GET_ONE_USER_SCHEMA),
@@ -37,41 +35,6 @@ router.post(GET_ONE_USER_URL,
                 message: 'Внутренняя ошибка сервера.'
             };
             console.log(err)
-        }
-    })
-    .post(GET_FIND_USERS_URL,
-    validator.validate(validator.GET_FIND_USERS_SCHEMA), 
-    async (ctx) => {
-
-        try {
-            
-            let data = ctx.request.body;
-            console.log(data);
-            //Проверка наличия токена
-            if (data.token) {       
-                let res = await usersQueries.getFindUsers(data)
-                if (res) {
-                    ctx.status = 200,
-                    ctx.body = {
-                        status: 'success',
-                        message: 'Пользователи получены!',
-                        users: res
-                    }
-                }
-            } else {
-                ctx.status = 403
-                ctx.body = {
-                    status: 'error',
-                    message: 'Доступ запрещен.'
-                }
-            }          
-        } catch (err) {
-            ctx.status = 500;
-            ctx.body = {
-                status: 'error',
-                message: 'Внутренняя ошибка сервера.'
-            };
-            console.log(err) 
         }
     });
 
