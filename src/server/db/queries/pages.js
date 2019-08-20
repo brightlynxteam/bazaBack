30b527c09796f85c22bab25a180927350b869998
SiriousHunter <46524538+SiriousHunter@users.noreply.github.com>
2019-08-20 15:23:27 +0300

    Update pages.js

--- src/server/db/queries/pages.js
+++ src/server/db/queries/pages.js
@@ -2,12 +2,13 @@ const knex = require('../connection');
 
 function editPage(data) {
     return knex('pages')
-        .returning(['id', 'topic', 'text','created_at', 'updated_at'])
         .where({ 'id': data.id })
         .update({
             topic: data.topic,
             text : data.text
         })
+        .returning(['id', 'topic', 'text','created_at', 'updated_at'])
+        .then(res => res[0]);
 
 }
 
