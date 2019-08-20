860b43819a00efcd8beaee17308faada9f8715a4
SiriousHunter <46524538+SiriousHunter@users.noreply.github.com>
2019-08-20 15:06:20 +0300

    Update services.js

--- src/server/db/queries/services.js
+++ src/server/db/queries/services.js
@@ -2,10 +2,10 @@ const knex = require('../connection');
 
 function editService(data) {
     return knex('services')
-        .returning(['id', 'name', 'description', 'price', 'info'])
         .where({'id': data.id})
         .update(data)
-        .then( res => res[0] )
+        .returning(['id', 'name', 'description', 'price', 'info'])
+        .then(res => res[0]);
 }
 
 module.exports = {
