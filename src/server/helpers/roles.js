fa2a8a53adb586269485ccb6cf9ce25fca58c9e4
Rudy Clark <rudy.uzb@gmail.com>
2019-08-21 13:27:31 +0500

    fix roles.js

--- src/server/helpers/roles.js
+++ src/server/helpers/roles.js
@@ -1,13 +1,5 @@
-function isEmpty(obj) {
-  for (key in obj) {
-    if (key in obj) return false;
-  }
-
-  return true;
-}
-
 function isAdmin(ctx, next) {
-  if (!isEmpty(ctx.state.user) && ctx.state.user.isAdmin)
+  if (ctx.state.user && ctx.state.user.isAdmin)
     return next();
 
   ctx.status = 403;
@@ -16,11 +8,11 @@ function isAdmin(ctx, next) {
     message: 'Доступ запрещен!'
   };
 
-  retrun;
+  return;
 }
 
 function isUser(ctx, next) {
-  if (!isEmpty(ctx.state.user)) return next();
+  if (ctx.state.user) return next();
 
   ctx.status = 403;
   ctx.body = {
