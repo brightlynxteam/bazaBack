36e285ccd4068e01272311d1e70fd718f9d6a03e
rainer048 <rainer048@gmail.com>
2019-08-23 18:37:02 +0300

    fix #71

--- src/server/db/migrations/07_pages.js
+++ src/server/db/migrations/07_pages.js
@@ -4,8 +4,14 @@ exports.up = function (knex, Promise) {
     return knex.schema
         .createTable('pages', function (table) {
             table.increments('id').primary();
-            table.string('topic', 255).unique();
-            table.text('text').notNullable();
+            table.string('text_id', 255).unique();
+            table.string('type', 255).notNullable();
+            table.string('title', 255).notNullable();
+            table.text('description').notNullable();
+            table.text('content').notNullable();
+            table.string('main_image', 255);
+            table.specificType('content_images', 'VARCHAR(255) []');
+            table.boolean('active').defaultTo(true).notNullable();
             table.integer('created_at').defaultTo(knex.raw('extract(epoch from now())'));
             table.integer('updated_at').defaultTo(knex.raw('extract(epoch from now())'));
         })
